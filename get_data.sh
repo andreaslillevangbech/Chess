efe9cb57263885e5349dbb87153a0c3195200d56
BobSaget <36662130+andreaslillevangbech@users.noreply.github.com>
2020-12-29 11:25:39 +0100

    now data getting works

--- get_data.sh
+++ get_data.sh
@@ -3,9 +3,9 @@ mkdir -p data
 mkdir -p processed
 
 # Get data from archive.org
-cd data
 curl -LO https://archive.org/download/KingBase2018/KingBase2018-pgn.zip
-cd ..
+cd data
+unzip ../KingBase2028-pgn.zip 
 
 # Process data
-./get_data.py
+./process_data.py
